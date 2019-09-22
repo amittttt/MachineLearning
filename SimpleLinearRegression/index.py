31ffbd76984b5ac3b8ac6a9efc2cfb896a2d2cfe
Mishra, AmitMishra <amit_mishra6@dell.com>
2019-09-22 17:46:33 +0530

    Adding Files

---
+++ SimpleLinearRegression/index.py
@@ -0,0 +1,7 @@
+import numpy as np  # mathamatical library
+import matplotlib.pyplot as plt  ## Plot chart
+import pandas as pd   # import data set and manage dataset
+
+# Importing the dataset
+
+dataset = pd.read_csv('Data.csv')
