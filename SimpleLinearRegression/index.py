c78bde55dc60875f1eb55297e7a2f4b6ff6812b8
Mishra, AmitMishra <amit_mishra6@dell.com>
2019-09-24 12:26:48 +0530

    adding more

--- SimpleLinearRegression/index.py
+++ SimpleLinearRegression/index.py
@@ -5,3 +5,34 @@ import pandas as pd   # import data set and manage dataset
 # Importing the dataset
 
 dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
+
+# Training and Test Set Data need to divide ::::
+from sklearn.model_selection import train_test_split
+x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=1/3,random_state=0)
+
+## Simple Linear Regression ::::
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+
+y_pred = regressor.predict(x_test)
+
+# Visualising the Training set results
+plt.scatter(x_train, y_train, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
